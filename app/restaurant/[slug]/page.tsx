f8c619dc7155067c9af350b437647738b6d770a3
Nahid-07 <nahidasif1998@gmail.com>
2023-05-09 11:13:36 +0600

    added some components

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -33,11 +33,11 @@ const restaurantDetailsPage = () => {
           <div className="bg-white w-[70%] rounded p-3 shadow">
             {/* RESAURANT NAVBAR */}
             <nav className="flex text-reg border-b pb-2">
-              <a href="" className="mr-7">
+              <Link href="/restaurant/1" className="mr-7">
                 {" "}
                 Overview{" "}
-              </a>
-              <Link href="" className="mr-7">
+              </Link>
+              <Link href="/restaurant/1/menu" className="mr-7">
                 {" "}
                 Menu{" "}
               </Link>
