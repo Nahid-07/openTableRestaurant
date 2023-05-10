cfe3e4406d8412957aa04e98eee38fa2e35f0835
Nahid-07 <nahidasif1998@gmail.com>
2023-05-10 11:53:21 +0600

    optimized code

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -15,12 +15,10 @@ const restaurantDetailsPage = () => {
             {/* RESAURANT NAVBAR */}
             <nav className="flex text-reg border-b pb-2">
               <Link href="/restaurant/1" className="mr-7">
-                {" "}
-                Overview{" "}
+                Overview
               </Link>
               <Link href="/restaurant/1/menu" className="mr-7">
-                {" "}
-                Menu{" "}
+                Menu
               </Link>
             </nav>
             {/* RESAURANT NAVBAR */} {/* TITLE */}
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,4 +1,5 @@
 import NavBar from "../component/NavBar";
+import Search from "../component/Search";
 
 const SearchPage = () => {
   return (
@@ -8,16 +9,7 @@ const SearchPage = () => {
         <NavBar></NavBar>
         {/* HEADER */}
         <div className="bg-gradient-to-r to-[#5f6984] from-[#0f1f47] p-2">
-          <div className="text-left text-lg py-3 m-auto flex justify-center">
-            <input
-              className="rounded  mr-3 p-2 w-[450px]"
-              type="text"
-              placeholder="State, city or town"
-            />
-            <button className="rounded bg-red-600 px-9 py-2 text-white">
-              Let's go
-            </button>
-          </div>
+          <Search></Search>
         </div>
         <div className="flex py-4 m-auto w-2/3 justify-between items-start">
           {/* SEARCH SIDE BAR */}
