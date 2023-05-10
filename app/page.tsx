d7a8b9bffae9bd77d0e9219f5f1df96dc86b1994
Nahid-07 <nahidasif1998@gmail.com>
2023-05-10 12:20:08 +0600

    optimized code

--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,20 @@ import Search from "./component/Search";
 
 export default function Home() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar></NavBar>
-
-        <main>
-          {/* HEADER */}
-          <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
-            <div className="text-center mt-10">
-              <h1 className="text-white text-5xl font-bold mb-2">
-                Find your table for any occasion
-              </h1>
-              <Search></Search>
-            </div>
-          </div>
-          {/* HEADER */} {/* CARDS */}
-          <div className="py-3 px-36 mt-10 flex flex-wrap">
-            <Card></Card>
-          </div>
-        </main>
-      </main>
+    <main>
+      {/* HEADER */}
+      <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
+        <div className="text-center mt-10">
+          <h1 className="text-white text-5xl font-bold mb-2">
+            Find your table for any occasion
+          </h1>
+          <Search></Search>
+        </div>
+      </div>
+      {/* HEADER */} {/* CARDS */}
+      <div className="py-3 px-36 mt-10 flex flex-wrap">
+        <Card></Card>
+      </div>
     </main>
   );
 }
--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -3,47 +3,44 @@ import NavBar from "@/app/component/NavBar";
 import Link from "next/link";
 const RestaurantMenuPage = () => {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar></NavBar>
-        <Header></Header>
-        {/* DESCRIPTION PORTION */}
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-          <div className="bg-white w-[100%] rounded p-3 shadow">
-            {/* RESAURANT NAVBAR */}
-            <nav className="flex text-reg border-b pb-2">
-              <Link href="/restaurant/1" className="mr-7">
-                Overview
-              </Link>
-              <a href="" className="mr-7">
-                Menu
-              </a>
-            </nav>
-            {/* RESAURANT NAVBAR */} {/* MENU */}
-            <main className="bg-white mt-5">
-              <div>
-                <div className="mt-4 pb-1 mb-1">
-                  <h1 className="font-bold text-4xl">Menu</h1>
-                </div>
-                <div className="flex flex-wrap justify-between">
-                  {/* MENU CARD */}
-                  <div className=" border rounded p-3 w-[49%] mb-3">
-                    <h3 className="font-bold text-lg">Surf and Turf</h3>
-                    <p className="font-light mt-1 text-sm">
-                      A well done steak with lobster and rice
-                    </p>
-                    <p className="mt-7">$80.00</p>
-                  </div>
-                  {/* MENU CARD */}
-                </div>
+    <>
+      <Header></Header>
+      {/* DESCRIPTION PORTION */}
+      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
+        <div className="bg-white w-[100%] rounded p-3 shadow">
+          {/* RESAURANT NAVBAR */}
+          <nav className="flex text-reg border-b pb-2">
+            <Link href="/restaurant/1" className="mr-7">
+              Overview
+            </Link>
+            <a href="" className="mr-7">
+              Menu
+            </a>
+          </nav>
+          {/* RESAURANT NAVBAR */} {/* MENU */}
+          <main className="bg-white mt-5">
+            <div>
+              <div className="mt-4 pb-1 mb-1">
+                <h1 className="font-bold text-4xl">Menu</h1>
               </div>
-            </main>
-            {/* MENU */}
-          </div>
+              <div className="flex flex-wrap justify-between">
+                {/* MENU CARD */}
+                <div className=" border rounded p-3 w-[49%] mb-3">
+                  <h3 className="font-bold text-lg">Surf and Turf</h3>
+                  <p className="font-light mt-1 text-sm">
+                    A well done steak with lobster and rice
+                  </p>
+                  <p className="mt-7">$80.00</p>
+                </div>
+                {/* MENU CARD */}
+              </div>
+            </div>
+          </main>
+          {/* MENU */}
         </div>
-        {/* DESCRIPTION PORTION */}
-      </main>
-    </main>
+      </div>
+      {/* DESCRIPTION PORTION */}
+    </>
   );
 };
 
